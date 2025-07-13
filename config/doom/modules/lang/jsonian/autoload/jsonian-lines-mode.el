74c145607b3a5c96d5b5cf5a22ccd51e16148c12
Ajai Khatri Nelson <ajai@ajai.dev>
2025-07-12 23:09:31 -0400

    Add command for viewing JSONL current line

--- config/doom/modules/lang/jsonian/autoload/jsonian-lines-mode.el
+++ config/doom/modules/lang/jsonian/autoload/jsonian-lines-mode.el
@@ -6,3 +6,26 @@
 (define-derived-mode +jsonian-lines-mode jsonian-mode "JSONL"
   "A major mode for editing JSON Lines (JSONL) files."
   :group 'akn)
+
+(map! :map +jsonian-lines-mode-map
+      :localleader
+      "v" #'+jsonian-lines/view-record)
+
+(defun +jsonian-lines/view-record ()
+  (interactive nil +jsonian-lines-mode)
+  (let* ((full-buf (current-buffer))
+         (full-buf-name (buffer-name full-buf))
+         (line-num (line-number-at-pos))
+         (line-start (pos-bol))
+         (line-end (pos-eol))
+         (new-buf (get-buffer-create (format "%s: record %d" full-buf-name line-num)))
+         (inhibit-read-only t))
+    (with-current-buffer new-buf
+      (erase-buffer)
+      (insert-buffer-substring full-buf line-start line-end)
+      (jsonian-mode)
+      (jsonian-format-region (point-min) (point-max))
+      (setq buffer-read-only t))
+    ;; (pop-to-buffer new-buf (cons #'display-buffer-in-side-window '((side . right))))
+    (+popup-buffer new-buf '((side . right)))))
+(defalias '+jsonian-lines/view-line #'+jsonian-lines/view-record)
