b3232295deabdc5df97a4b3b02b55f4731e7653e
Ajai Khatri Nelson <ajai@ajai.dev>
2025-08-30 17:07:25 -0400

    bump: :term mistty
    szermatt/mistty@8bcb9c056e49 -> szermatt/mistty@bfb17611cff6

--- config/doom/modules/term/mistty/packages.el
+++ config/doom/modules/term/mistty/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; term/mistty/packages.el
 
-(package! mistty :pin "8bcb9c056e49dfbb546fe68a27d64c55e7691883")
+(package! mistty :pin "bfb17611cff6c845270050a0756a38489cdf4ed6")
