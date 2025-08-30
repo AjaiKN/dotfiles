3b799e5c1ebacc20249fa2df8f1927cd6dc497ae
Ajai Khatri Nelson <ajai@ajai.dev>
2025-08-30 17:06:03 -0400

    bump: :input reverse-im
    a13/reverse-im.el@dfb169b08e09 -> a13/reverse-im.el@20d5f0514a76

--- config/doom/modules/input/reverse-im/packages.el
+++ config/doom/modules/input/reverse-im/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/reverse-im/packages.el
 
-(package! reverse-im :pin "dfb169b08e09be6ab113a7d423f79b1775a9334f")
+(package! reverse-im :pin "20d5f0514a761f0a06284b2adf0baf4bf7b93db2")
