09bc0d22ec1d332f7dba289354bd8813f40359c6
Ajai Khatri Nelson <ajai@ajai.dev>
2025-08-30 17:07:04 -0400

    bump: :os exwm
    LemonBreezes/exwm-evil@9af0f07d61bf -> LemonBreezes/exwm-evil@722947890aba
    emacs-exwm/exwm@9065e090b62f -> emacs-exwm/exwm@1d68e170d581
    emacs-exwm/xelb@e9ff30f6e8d0 -> emacs-exwm/xelb@6dfda93a39b6

--- config/doom/modules/os/exwm/packages.el
+++ config/doom/modules/os/exwm/packages.el
@@ -6,12 +6,12 @@
 (package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib") :pin "de3d8379b631eef57e683af0dec65c8d4fb0350f")
 
 (package! xelb :recipe
-  (:host github :repo "emacs-exwm/xelb") :pin "e9ff30f6e8d0a1e650db1eb2a9194eeb11fbe544")
+  (:host github :repo "emacs-exwm/xelb") :pin "6dfda93a39b6cc1919779f71dc96ef353061aa3e")
 (package! exwm :recipe
-  (:host github :repo "emacs-exwm/exwm") :pin "9065e090b62f50ab85eb0a4eacfd77d151afadc3")
+  (:host github :repo "emacs-exwm/exwm") :pin "1d68e170d58176d931200576e0322c57fa2326df")
 (package! exwm-mff :pin "89206f2e3189f589c27c56bd2b6203e906ee7100")
 (when (modulep! :editor evil +everywhere)
   (package! exwm-evil
-    :recipe (:host github :repo "LemonBreezes/exwm-evil") :pin "9af0f07d61bfbf813cbc4fcec3c108dd0cee3874")
+    :recipe (:host github :repo "LemonBreezes/exwm-evil") :pin "722947890abaa3483f71aa5f9082897e939c0eb9")
   (package! exwm-firefox-evil :pin "c87d601de9bad4d3cbf41c69281073b465a66769")
   (package! app-launcher :recipe (:host github :repo "SebastienWae/app-launcher") :pin "d5015e394b0a666a8c7c4d4bdf786266e773b145"))
