f49b0cf356241f3df4c444d1b26d4548459f587e
Ajai Khatri Nelson <ajai@ajai.dev>
2025-09-22 06:03:55 -0400

    Make sure leader keys are set after modules config

--- config/doom/modules/config/akn-bindings/init.el
+++ config/doom/modules/config/akn-bindings/init.el
@@ -1,14 +1,21 @@
 ;;; config/akn-bindings/init.el -*- lexical-binding: t; -*-
 
 (when (modulep! +leader-keys)
-  (if (modulep! :editor evil)
-      (setq! doom-leader-key "SPC"
-             doom-leader-alt-key "M-SPC"
-             doom-localleader-key "C-c"
-             doom-localleader-alt-key "C-c")
-    (setq! doom-leader-alt-key "M-SPC" ;"C-."
-           doom-localleader-alt-key "C-c"
-           cursor-type 'bar))
+  (defun akn/define-leader-keys ()
+    (if (modulep! :editor evil)
+        (setq! doom-leader-key "SPC"
+               doom-leader-alt-key "M-SPC"
+               doom-localleader-key "C-c"
+               doom-localleader-alt-key "C-c")
+      (setq! doom-leader-alt-key "M-SPC" ;"C-."
+             doom-localleader-alt-key "C-c"
+             cursor-type 'bar))
 
-  ;; also see https://github.com/TheJJ/conffiles/blob/master/.config/doom/keys.el
-  (setq doom-leader-alt-key-states '(normal visual insert emacs motion operator replace global)))
+    ;; also see https://github.com/TheJJ/conffiles/blob/master/.config/doom/keys.el
+    (setq doom-leader-alt-key-states '(normal visual insert emacs motion operator replace global)))
+
+  (akn/define-leader-keys)
+  ;; In non-evil mode, ~/.config/emacs/modules/config/default/+emacs-bindings.el
+  ;; gets loaded, and that file sets `doom-leader-alt-key' and
+  ;; `doom-localleader-alt-key', so we need to set them again.
+  (add-hook 'doom-after-modules-config-hook #'akn/define-leader-keys))
