e7dbea8c4a40153ac495d15c107ae03d7a097618
Ajai Khatri Nelson <ajai@ajai.dev>
2025-09-30 00:38:22 -0400

    bump: :lang typst
    meow_king/typst-ts-mode@99fd3d4d889c -> meow_king/typst-ts-mode@fa03e477dfc5

--- config/doom/modules/lang/typst/packages.el
+++ config/doom/modules/lang/typst/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/typst/packages.el
 
-(package! typst-ts-mode :pin "99fd3d4d889c0c7365563905d1fa0e08b7e5082b"
+(package! typst-ts-mode :pin "fa03e477dfc57ea498ef9a624b2ec7bf655dc25c"
   :recipe (:type git :host codeberg :repo "meow_king/typst-ts-mode"))
 
 (package! typst-preview :pin "4f3decb8d9b85b1a06b0dfbd0fccaa0d2d2c24b4"
