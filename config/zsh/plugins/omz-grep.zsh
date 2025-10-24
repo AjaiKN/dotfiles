a283fc0233c15153f700af670749c47ea6264cce
Ajai Khatri Nelson <ajai@ajai.dev>
2025-10-24 14:38:18 -0400

    speed up omz grep

--- config/zsh/plugins/omz-grep.zsh
+++ config/zsh/plugins/omz-grep.zsh
@@ -22,44 +22,39 @@
 # SOFTWARE.
 
 ## COPIED FROM OH-MY-ZSH
-__GREP_CACHE_FILE="$ZSH_CACHE_DIR"/grep-alias
 
-# See if there's a cache file modified in the last day
-__GREP_ALIAS_CACHES=("$__GREP_CACHE_FILE"(Nm-1))
-if [[ -n "$__GREP_ALIAS_CACHES" ]]; then
-    source "$__GREP_CACHE_FILE"
-else
+_akn_omz_grep() {
+    unalias grep
+    unalias egrep
+    unalias fgrep
+
     grep-flags-available() {
         command grep "$@" "" &>/dev/null <<< ""
     }
 
     # Ignore these folders (if the necessary grep flags are available)
-    EXC_FOLDERS="{.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv}"
+    local EXC_FOLDERS="{.bzr,CVS,.git,.hg,.svn,.idea,.tox,.venv,venv}"
 
     # Check for --exclude-dir, otherwise check for --exclude. If --exclude
     # isn't available, --color won't be either (they were released at the same
     # time (v2.5): https://git.savannah.gnu.org/cgit/grep.git/tree/NEWS?id=1236f007
+    local -a grep_options=()
     if grep-flags-available --color=auto --exclude-dir=.cvs; then
-        GREP_OPTIONS="--color=auto --exclude-dir=$EXC_FOLDERS"
+        grep_options=(--color=auto --exclude-dir="$EXC_FOLDERS")
     elif grep-flags-available --color=auto --exclude=.cvs; then
-        GREP_OPTIONS="--color=auto --exclude=$EXC_FOLDERS"
+        grep_options=(--color=auto --exclude="$EXC_FOLDERS")
     fi
 
-    if [[ -n "$GREP_OPTIONS" ]]; then
-        # export grep, egrep and fgrep settings
-        alias grep="grep $GREP_OPTIONS"
-        alias egrep="grep -E"
-        alias fgrep="grep -F"
-
-        # write to cache file if cache directory is writable
-        if [[ -w "$ZSH_CACHE_DIR" ]]; then
-            alias -L grep egrep fgrep >| "$__GREP_CACHE_FILE"
-        fi
+    if [[ -n "$grep_options" ]]; then
+        alias grep="grep $grep_options"
     fi
 
-    # Clean up
-    unset GREP_OPTIONS EXC_FOLDERS
     unfunction grep-flags-available
-fi
+    unfunction _akn_omz_grep
 
-unset __GREP_CACHE_FILE __GREP_ALIAS_CACHES
+    command grep "${grep_options[@]}" "$@"
+}
+
+alias grep="_akn_omz_grep"
+alias egrep="grep -E"
+alias fgrep="grep -F"
