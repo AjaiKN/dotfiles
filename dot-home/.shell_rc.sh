ecbba77a79ffc3701a35f1d07feb4b2c04f637a6
Ajai Khatri Nelson <ajai@ajai.dev>
2025-10-31 00:21:35 -0400

    fix alias

--- dot-home/.shell_rc.sh
+++ dot-home/.shell_rc.sh
@@ -473,8 +473,8 @@ alias tar-create='tar caf'
 alias tar-ls='tar tvf'
 # bsdtar "can extract from tar, pax, cpio, zip, jar, ar, xar, rpm, 7-zip, and ISO 9660 cdrom images and can create tar, pax, cpio, ar, zip, 7-zip, and shar archives."
 alias archive='bsdtar caf'
-alias unarchive='bsdtar xf'
-alias archive-ls='bsdtar tvf'
+alias unarchive='bsdtar xvf' # e[x]tract [v]erbose [f]ile
+alias archive-ls='bsdtar tvf' # lis[t] [v]erbose [f]ile
 
 if command -v wget >/dev/null 2>&1; then
 	alias download='wget'
