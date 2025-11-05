521b04fd0d2aa123505df2a11b7a073604b480c8
Ajai Khatri Nelson <ajai@ajai.dev>
2025-11-05 13:20:57 -0500

    linux stuff

--- install-permissions.sh
+++ install-permissions.sh
@@ -7,7 +7,8 @@ cd "$HOME" || exit 1
 chmod go-rwx prog 2>/dev/null
 chmod go-rwx .ssh .gnupg 2>/dev/null
 chmod go-rwx Trash .Trash "${XDG_DATA_HOME:-.local/share}"/Trash .local/share/Trash 2>/dev/null
-chmod go-rwx Desktop Documents Downloads Finance Library Movies Music Pictures 2>/dev/null
+chmod go-rwx Desktop Documents Downloads Finance Library Movies Music Pictures Vidoes 2>/dev/null
+chmod go-rwx "${XDG_DESKTOP_DIR:-Desktop}" "${XDG_DOCUMENTS_DIR:-Documents}" "${XDG_DOWNLOAD_DIR:-Downloads}" "${XDG_MUSIC_DIR:-Music}" "${XDG_PICTURES_DIR:-Pictures}" "${XDG_VIDEOS_DIR:-Videos}" 2>/dev/null
 chmod go-rwx work projects project github 2>/dev/null
 chmod go-rwx ./*.gpg .*.gpg 2>/dev/null
 chmod go-rwx ./*history .*history 2>/dev/null
