e759346895ba1ada2c41fe3cc390c665abde9797
Ajai Khatri Nelson <ajai@ajai.dev>
2025-11-05 12:48:33 -0500

    Don't print message

--- config/zsh/plugins/joke/zim-mise-modified/init.zsh
+++ config/zsh/plugins/joke/zim-mise-modified/init.zsh
@@ -9,6 +9,6 @@
   local compfile=$1/functions/_mise
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command complete --shell zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'mise'. Regenerated completions."
+    # print -u2 -PR "* Detected a new version 'mise'. Regenerated completions."
   fi
 } ${0:h}
