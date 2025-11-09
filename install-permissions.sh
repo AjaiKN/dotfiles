596ad74b76ad4ef6d5f1f9ffbeaec072b4f8cbdb
Ajai Khatri Nelson <ajai@ajai.dev>
2025-11-09 13:33:29 -0500

    Improve install script output

--- install-permissions.sh
+++ install-permissions.sh
@@ -1,21 +1,50 @@
 #!/bin/sh
 
-set -x
 cd "$HOME" || exit 1
 
-# chmod go-rwx "$HOME"
-chmod go-rwx prog 2>/dev/null
-chmod go-rwx .ssh .gnupg 2>/dev/null
-chmod go-rwx Trash .Trash "${XDG_DATA_HOME:-.local/share}"/Trash .local/share/Trash 2>/dev/null
-chmod go-rwx Desktop Documents Downloads Finance Library Movies Music Pictures Vidoes 2>/dev/null
-chmod go-rwx "${XDG_DESKTOP_DIR:-Desktop}" "${XDG_DOCUMENTS_DIR:-Documents}" "${XDG_DOWNLOAD_DIR:-Downloads}" "${XDG_MUSIC_DIR:-Music}" "${XDG_PICTURES_DIR:-Pictures}" "${XDG_VIDEOS_DIR:-Videos}" 2>/dev/null
-chmod go-rwx work projects project github 2>/dev/null
-chmod go-rwx ./*.gpg .*.gpg 2>/dev/null
-chmod go-rwx ./*history .*history 2>/dev/null
-chmod go-rwx ./*history_backup .*history_backup 2>/dev/null
-chmod go-rwx Zotero ZotMoov 2>/dev/null
-chmod go-rwx .config .local .local/bin .local/share .local/state .cache 2>/dev/null
-chmod go-rwx "${XDG_CONFIG_HOME:-.cache}" "${XDG_STATE_HOME:-.local/state}" "${XDG_DATA_HOME:-.local/share}" "${XDG_CACHE_HOME:-.cache}" 2>/dev/null
-chmod go-rwx ./*rc .*rc 2>/dev/null
+filter_output() {
+	while IFS= read -r line; do
+		case "$line" in
+			*'retained as'*) ;;
+			*'could not be accessed'*) ;;
+			*'No such file or directory'*) ;;
+			*) printf '%s\n' "$line" ;;
+		esac
+	done
+}
 
-chmod go-rwx "$DOTFILES"
+chmod="chmod"
+
+# -v isn't part of POSIX
+has_verbose_option=
+if "$chmod" --help 2>&1 | grep -E '\[-[^ ]*v[^ ]*\]| -v[, ]|^-v' >/dev/null; then
+	has_verbose_option=1
+fi
+
+mychmod() {
+	if [ -n "$has_verbose_option" ]; then
+		"$chmod" -vv "$@" 2>/dev/null | filter_output
+	else
+		(
+			set -x
+			"$chmod" "$@" 2>&1
+		) | filter_output
+	fi
+}
+
+# mychmod go-rwx "$HOME"
+mychmod go-rwx prog
+mychmod go-rwx .ssh .gnupg
+mychmod go-rwx Trash .Trash "${XDG_DATA_HOME:-.local/share}"/Trash .local/share/Trash
+mychmod go-rwx Desktop Documents Downloads Finance Library Movies Music Pictures Vidoes
+mychmod go-rwx "${XDG_DESKTOP_DIR:-Desktop}" "${XDG_DOCUMENTS_DIR:-Documents}" "${XDG_DOWNLOAD_DIR:-Downloads}" "${XDG_MUSIC_DIR:-Music}" "${XDG_PICTURES_DIR:-Pictures}" "${XDG_VIDEOS_DIR:-Videos}"
+mychmod go-rwx work projects project github
+mychmod go-rwx ./*.gpg .*.gpg
+mychmod go-rwx ./*history .*history
+mychmod go-rwx ./*history_backup .*history_backup
+mychmod go-rwx Zotero ZotMoov
+mychmod go-rwx .config .local .local/bin .local/share .local/state .cache
+mychmod go-rwx "${XDG_CONFIG_HOME:-.cache}" "${XDG_STATE_HOME:-.local/state}" "${XDG_DATA_HOME:-.local/share}" "${XDG_CACHE_HOME:-.cache}"
+mychmod go-rwx ./*rc .*rc
+
+mychmod go-rwx "$DOTFILES"
