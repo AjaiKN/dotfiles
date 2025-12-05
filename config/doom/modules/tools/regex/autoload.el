f1ed14f6b844cdfea3baf7f1bd8b206d796d8717
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-04 21:54:01 -0500

    feat(regex): add prx macro (useful for eshell)

--- config/doom/modules/tools/regex/autoload.el
+++ config/doom/modules/tools/regex/autoload.el
@@ -40,3 +40,23 @@
 (defun +regex/xr-try-replace-at-point (&optional should-message)
   (interactive current-prefix-arg)
   (+regex/xr-at-point (not buffer-read-only) (or buffer-read-only should-message)))
+
+;;;###autoload
+(defmacro prx (&rest expressions)
+  "Convert the rx-compatible regular EXPRESSIONS to PCRE.
+Most shell applications accept Perl Compatible Regular Expressions.
+
+From https://howardism.org/Technical/Emacs/eshell-why.html and
+https://github.com/howardabrams/hamacs/blob/main/ha-eshell.org#regular-expressions (CC0)."
+  `(rx-let ((integer (1+ digit))
+            (float   (seq integer "." integer))
+            (b256    (seq (optional (or "1" "2"))
+                          (regexp "[0-9]\\{1,2\\}")))
+            (ipaddr  (seq b256 "." b256 "." b256 "." b256))
+            (time    (seq digit (optional digit) ":" (= 2 digit) (optional ":" (= 2 digit))))
+            (email   (seq (1+ (regexp "[^,< ]")) "@" (1+ (seq (1+ (any alnum "-"))) ".") (1+ alnum)))
+            (date    (seq (= 2 digit) (or "/" "-") (= 2 digit) (or "/" "-") (= 4 digit)))
+            (ymd     (seq (= 4 digit) (or "/" "-") (= 2 digit) (or "/" "-") (= 2 digit)))
+            (uuid    (seq (= 8 hex) "-" (= 3 (seq (= 4 hex) "-")) (= 12 hex)))
+            (guid    (seq uuid)))
+     (rxt-elisp-to-pcre (rx ,@expressions))))
