9074025299aae6177056546ec18b8a044224e42e
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-08 01:25:21 -0500

    tweak(mason): add mason bin to PATH before loading mason

--- config/doom/modules/tools/mason/config.el
+++ config/doom/modules/tools/mason/config.el
@@ -1,4 +1,10 @@
 ;;; tools/mason/config.el -*- lexical-binding: t; -*-
 
+;; Add Mason's bin directory to PATH right away.
+(defvar mason-dir (expand-file-name "mason" doom-cache-dir))
+(let ((bin-dir (expand-file-name "bin" mason-dir)))
+  (setenv "PATH" (concat bin-dir ":" (getenv "PATH")))
+  (add-to-list 'exec-path bin-dir))
+
 (use-package! mason
   :hook (doom-first-input . mason-ensure))
