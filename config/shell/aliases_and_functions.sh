c771605fba6c301451a1f0d457eca800c21f4680
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-16 16:09:31 -0500

    feat(shell): https://github.com/ohmyzsh/ohmyzsh/commit/2525dae6613652ec9cb572bdc2fdf80ef837a967

--- config/shell/aliases_and_functions.sh
+++ config/shell/aliases_and_functions.sh
@@ -45,16 +45,27 @@ fi
 
 #from ohmyzsh
 
-# Check if main exists and use instead of master
+# Get the default branch name from common branch names or fallback to remote HEAD
 function git_main_branch() {
 	command git rev-parse --git-dir &>/dev/null || return
-	local ref
+
+	local remote ref
+
 	for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
 		if command git show-ref -q --verify "$ref"; then
 			echo "${ref:t}"
 			return 0
 		fi
 	done
+
+	# Fallback: try to get the default branch from remote HEAD symbolic refs
+	for remote in origin upstream; do
+		ref=$(command git rev-parse --abbrev-ref $remote/HEAD 2>/dev/null)
+		if [[ $ref == $remote/* ]]; then
+			echo "${ref#"$remote/"}"; return 0
+		fi
+	done
+
 	# If no main branch was found, fall back to master but return error
 	echo master
 	return 1
