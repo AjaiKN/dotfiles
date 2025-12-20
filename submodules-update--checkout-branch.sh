452cb007092751fea470e6976128c945a2dd1436
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-19 22:59:28 -0500

    fix(install): make sure the branch exists

--- submodules-update--checkout-branch.sh
+++ submodules-update--checkout-branch.sh
@@ -8,12 +8,18 @@ if git symbolic-ref -q HEAD >/dev/null; then
 fi
 
 if branch=$(git config -f "$toplevel"/.gitmodules submodule."$name".branch); then
-	head_commit=$(git rev-parse HEAD)
-	if [ "$(git rev-parse "origin/$branch")" = "$head_commit" ]; then
+	head_commit=$(git rev-parse --abbrev-ref=strict HEAD)
+	if ! git rev-parse --abbrev-ref=strict "origin/$branch" >/dev/null 2>&1; then
+		git fetch --porcelain --update-shallow origin "$branch":"origin/$branch"
+	fi
+	if ! git rev-parse --abbrev-ref=strict "$branch" >/dev/null 2>&1; then
+		git branch "$branch" "origin/$branch"
+	fi
+	if [ "$(git rev-parse --abbrev-ref=strict "origin/$branch")" = "$head_commit" ]; then
 		"$DOTFILES"/bin/git-ff "$branch" "origin/$branch" 2>/dev/null ||
 			echo "Can't fast-forward $branch to origin/$branch"
 	fi
-	if [ "$(git rev-parse "$branch")" = "$head_commit" ]; then
+	if [ "$(git rev-parse --abbrev-ref=strict "$branch")" = "$head_commit" ]; then
 		switch_to=$branch
 	fi
 fi
