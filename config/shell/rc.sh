79d6948b5b4b2ff022efdec273b5695a1326f887
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-22 06:19:10 -0500

    shell: remove thefuck

--- config/shell/rc.sh
+++ config/shell/rc.sh
@@ -87,26 +87,6 @@ alias e='f -e "$EDITOR"'
 	>&2 echo_if_interactive "Warning: package.json found in a location where it probably shouldn't be"
 fi &)
 
-## thefuck
-# output of `thefuck --alias`
-# could also use the ohmyzsh plugin
-fuck () {
-	TF_PYTHONIOENCODING=$PYTHONIOENCODING;
-	export TF_SHELL=zsh;
-	export TF_ALIAS=fuck;
-	TF_SHELL_ALIASES=$(alias);
-	export TF_SHELL_ALIASES;
-	TF_HISTORY="$(fc -ln -10)";
-	export TF_HISTORY;
-	export PYTHONIOENCODING=utf-8;
-	TF_CMD=$(
-		thefuck THEFUCK_ARGUMENT_PLACEHOLDER $@
-				) && eval $TF_CMD;
-	unset TF_HISTORY;
-	export PYTHONIOENCODING=$TF_PYTHONIOENCODING;
-	test -n "$TF_CMD" && print -s $TF_CMD
-}
-
 ## wezterm integration
 if [ -n "$WEZTERM_EXECUTABLE_DIR" ]; then
 	WEZTERM_SHELL_INTEGRATION="$WEZTERM_EXECUTABLE_DIR"/../Resources/wezterm.sh
