a52a4786c2dcfb968b60aee596368efe6772c0e7
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-22 22:59:30 -0600

    yazi: use icons

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,5 +1,9 @@
 th.git = th.git or {}
-th.git.modified_sign = "M"
-th.git.deleted_sign = "D"
+-- th.git.ignored_sign   = "I"
+-- th.git.untracked_sign = "?"
+-- th.git.modified_sign  = "M"
+-- th.git.added_sign     = "S"
+-- th.git.deleted_sign   = "D"
+-- th.git.updated_sign   = "updated"
 
 require("git"):setup()
