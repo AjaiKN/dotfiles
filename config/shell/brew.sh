1133f3ff03b04be8da99b4c149efd59c5f03f9e9
Ajai Khatri Nelson <ajai@ajai.dev>
2026-01-01 23:48:08 -0500

    shell: refactor PATH stuff

---
+++ config/shell/brew.sh
@@ -0,0 +1,41 @@
+# shellcheck shell=sh
+
+# brew
+if [ -z "$HOMEBREW_PREFIX" ] || ! [ -e "$HOMEBREW_PREFIX"/bin/brew ]; then
+	if [ -e /System ]; then
+		# macOS
+		if [ -e /opt/homebrew/bin/brew ]; then HOMEBREW_PREFIX=/opt/homebrew
+		elif [ -e /usr/local/bin/brew ]; then HOMEBREW_PREFIX=/usr/local
+		fi
+	else
+		if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then HOMEBREW_PREFIX=/home/linuxbrew/.linuxbrew
+		elif [ -e "$HOME"/.linuxbrew/bin/brew ]; then HOMEBREW_PREFIX="$HOME"/.linuxbrew
+		fi
+	fi
+	export HOMEBREW_PREFIX
+fi
+
+if [ -x "$HOMEBREW_PREFIX"/bin/brew ]; then
+	eval "$("$HOMEBREW_PREFIX"/bin/brew shellenv)"
+	#OR:
+	# export HOMEBREW_CELLAR="$HOMEBREW_PREFIX"/Cellar
+	# if [ -e "$HOMEBREW_PREFIX"/Homebrew/Library ]; then
+	# 	export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX"/Homebrew
+	# else
+	# 	export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX"
+	# fi
+	# if [ -d "$HOMEBREW_PREFIX" ]; then
+	# 	export PATH="$HOMEBREW_PREFIX"/bin:"$HOMEBREW_PREFIX"/sbin:"$PATH"
+	# 	[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+	# 	export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
+	# fi
+	# # TODO: zsh fpath
+
+	if [ -e "$HOMEBREW_PREFIX"/opt/llvm/lib/c++ ]; then
+		# use homebrew's llvm
+		export LDFLAGS="-L$HOMEBREW_PREFIX/opt/llvm/lib/c++ -L$HOMEBREW_PREFIX/opt/llvm/lib -lunwind"
+		export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH:$HOMEBREW_PREFIX/opt/python/libexec/bin"
+		export LDFLAGS="-L$HOMEBREW_PREFIX/opt/llvm/lib"
+		export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/llvm/include"
+	fi
+fi
--- config/shell/path.sh
+++ config/shell/path.sh
@@ -1,78 +1,10 @@
 # shellcheck shell=sh
 
-## lowest priority
-export PLAN9="$HOME/prog/plan9port"
-
-export PATH="$PATH:\
-$HOME/.antigravity/antigravity/bin:\
-/opt/R/arm64/gfortran/bin:\
-/Applications/Firefox.app/Contents/MacOS:\
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support:\
-$PLAN9/bin\
-"
-
-## system = low-mid priority
-
-## mid priority
-export PATH="\
-/opt/homebrew/bin:\
-/snap/bin:\
-/usr/local/bin:\
-/usr/local/sbin:\
-/opt/local/bin:\
-/opt/local/sbin:\
-/usr/bin:\
-/bin:\
-/usr/sbin:\
-/sbin:\
-$PATH"
-
-## brew (mid-high priority)
-if [ -z "$HOMEBREW_PREFIX" ] || ! [ -e "$HOMEBREW_PREFIX"/bin/brew ]; then
-	if [ -e /System ]; then
-		# macOS
-		if [ -e /opt/homebrew/bin/brew ]; then HOMEBREW_PREFIX=/opt/homebrew
-		elif [ -e /usr/local/bin/brew ]; then HOMEBREW_PREFIX=/usr/local
-		fi
-	else
-		if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then HOMEBREW_PREFIX=/home/linuxbrew/.linuxbrew
-		elif [ -e "$HOME"/.linuxbrew/bin/brew ]; then HOMEBREW_PREFIX="$HOME"/.linuxbrew
-		fi
-	fi
-	export HOMEBREW_PREFIX
-fi
-
-if [ -x "$HOMEBREW_PREFIX"/bin/brew ]; then
-	eval "$("$HOMEBREW_PREFIX"/bin/brew shellenv)"
-	#OR:
-	# export HOMEBREW_CELLAR="$HOMEBREW_PREFIX"/Cellar
-	# if [ -e "$HOMEBREW_PREFIX"/Homebrew/Library ]; then
-	# 	export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX"/Homebrew
-	# else
-	# 	export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX"
-	# fi
-	# if [ -d "$HOMEBREW_PREFIX" ]; then
-	# 	export PATH="$HOMEBREW_PREFIX"/bin:"$HOMEBREW_PREFIX"/sbin:"$PATH"
-	# 	[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
-	# 	export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
-	# fi
-	# # TODO: zsh fpath
-
-	if [ -e "$HOMEBREW_PREFIX"/opt/llvm/lib/c++ ]; then
-		# use homebrew's llvm
-		export LDFLAGS="-L$HOMEBREW_PREFIX/opt/llvm/lib/c++ -L$HOMEBREW_PREFIX/opt/llvm/lib -lunwind"
-		export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH:$HOMEBREW_PREFIX/opt/python/libexec/bin"
-		export LDFLAGS="-L$HOMEBREW_PREFIX/opt/llvm/lib"
-		export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/llvm/include"
-	fi
-fi
-
-## highest priority
-
-export PATH="\
+akn_path_high_priority="\
 $HOME/.local/bin:\
 $HOME/bin:\
-$DOTFILES/private/bin:$DOTFILES/bin:\
+$DOTFILES/private/bin:
+$DOTFILES/bin:\
 $HOME/.local/share/mise/shims:\
 $HOME/.config/emacs/bin:\
 /run/wrappers/bin:\
@@ -84,8 +16,41 @@ $HOME/.local/state/nix/profile/bin:\
 /run/current-system/sw/bin:\
 $HOME/.cargo/bin:\
 $HOME/.poetry/bin:\
-/Applications/Coq-Platform~8.16~2022.09.app/Contents/Resources/bin:\
-$PATH"
+/Applications/Coq-Platform~8.16~2022.09.app/Contents/Resources/bin"
+
+# then brew
+
+akn_path_mid_priority="\
+/opt/homebrew/bin:\
+/snap/bin:\
+/usr/local/bin:\
+/usr/local/sbin:\
+/opt/local/bin:\
+/opt/local/sbin:\
+/usr/bin:\
+/bin:\
+/usr/sbin:\
+/sbin"
+
+# then original $PATH
+
+akn_path_lowest_priority="\
+$HOME/.antigravity/antigravity/bin:\
+/opt/R/arm64/gfortran/bin:\
+/Applications/Firefox.app/Contents/MacOS:\
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support:\
+$HOME/prog/plan9port/bin\
+"
+
+## putting it all together
+
+export PATH="$akn_path_mid_priority:$PATH:$akn_path_lowest_priority"
+# shellcheck source=./brew.sh
+. "$HOME/.config/shell/brew.sh"
+export PATH="$akn_path_high_priority:$PATH"
+
+unset akn_path_high_priority akn_path_mid_priority akn_path_lowest_priority
+
 
 ## Secure PATH
 if [ -x "$DOTFILES/scripts/secure_path" ]; then
