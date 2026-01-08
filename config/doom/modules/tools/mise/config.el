597bd58a3ff56b7173c2842b1e624f597ec3edec
Ajai Khatri Nelson <ajai@ajai.dev>
2026-01-07 20:21:50 -0500

    emacs: mise: enable debug

--- config/doom/modules/tools/mise/config.el
+++ config/doom/modules/tools/mise/config.el
@@ -1,4 +1,6 @@
 ;;; tools/mise/config.el -*- lexical-binding: t; -*-
 
 (use-package! mise
-  :ghook ('doom-first-file-hook #'global-mise-mode))
+  :ghook ('doom-first-file-hook #'global-mise-mode)
+  :config
+  (setq! mise-debug t))
