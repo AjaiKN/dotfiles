6d5cc85dce2f104dcf089294227cc1961c4f72b7
Ajai Khatri Nelson <ajai@ajai.dev>
2026-01-07 22:05:51 -0500

    emacs: +mise-trust-mode

--- config/doom/modules/tools/mise/config.el
+++ config/doom/modules/tools/mise/config.el
@@ -3,4 +3,25 @@
 (use-package! mise
   :ghook ('doom-first-file-hook #'global-mise-mode)
   :config
-  (setq! mise-debug t))
+  (setq! mise-debug t)
+  (akn/run-command `("mise" "trust" ,(file-truename "~/.config/mise/config.toml"))
+                   :boring t))
+
+;; If the user saves a mise config file, we can trust it.
+(def-project-mode! +mise-trust-mode
+  :match (rx (seq "/"
+                  (or (seq (? ".")
+                           (or "mise.local.toml"
+                               "mise.toml"
+                               "mise/config.toml"
+                               (seq "mise/conf.d/" (* (not "/")) ".toml")))
+                      ".tool-versions"
+                      (seq "." (* (not "/")) "-version")
+                      "main.tf" ".sdkmanrc" ".nvmrc" ".yvmrc")
+                  eos))
+  :on-enter (add-hook! 'after-save-hook :local
+              (defun +mise-trust-h ()
+                "Whenever the user saves this file, run mise trust on it."
+                (akn/run-command `("mise" "trust" ,(expand-file-name buffer-file-truename))
+                                 ;; :output-buffer " *mise-trust*"
+                                 :boring t))))
