8ec3c624b13e22583c31f3fd6a5d2a29f43770d1
Ajai Khatri Nelson <ajai@ajai.dev>
2026-01-19 18:02:59 -0500

    shell: remove unneeded line from mise plugin

--- config/zsh/plugins/mise.zsh
+++ config/zsh/plugins/mise.zsh
@@ -8,7 +8,7 @@ unset __MISE_ORIG_PATH # Leaving this set has resulted in some confusing behavio
 
 eval "$(command mise activate zsh)"
 # source <($command activate zsh)
-source <(command mise hook-env -s zsh)
+# source <(command mise hook-env -s zsh)
 
 # PATH="$mise_oldpath"
 # unset mise_oldpath
