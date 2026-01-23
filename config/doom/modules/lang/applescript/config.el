73fa3d9542a6f6e798603f04b199e5febda2745e
Ajai Khatri Nelson <ajai@ajai.dev>
2026-01-23 15:33:25 -0500

    emacs: applescript: switch from applescript-mode to my new apples-mode fork

--- config/doom/modules/lang/applescript/config.el
+++ config/doom/modules/lang/applescript/config.el
@@ -1,22 +1,9 @@
 ;;; lang/applescript/config.el -*- lexical-binding: t; -*-
 
-(use-package! applescript-mode
-  :commands (+applescript-mode)
+(use-package! apples-mode
+  :defer t
   :config
-  (define-derived-mode +applescript-mode prog-mode "AppleScript"
-    "A replacement wrapper for `applescript-mode'.
-
-This uses `define-derived-mode', as opposed to the old style of defining
-major modes, where regular functions were used."
-    :group 'applescript
-    (akn/letf! ((major-mode major-mode) ; don't change `major-mode' from `+applescript-mode' to `applescript-mode'
-                (mode-name mode-name)
-                (#'kill-all-local-variables #'ignore)) ; with define-derived-mode, there's no need to kill-all-local-variables
-      (applescript-mode)))
-  (derived-mode-add-parents '+applescript-mode '(applescript-mode))
-  :init
-  (when (boundp 'major-mode-remap-defaults)
-    (setf (alist-get 'applescript-mode major-mode-remap-defaults) #'+applescript-mode)))
+  (setq! apples-indent-offset (default-value 'tab-width)))
 
 ;; By default, Script Editor.app saves AppleScript in the compiled ".scpt"
 ;; format (https://apple.stackexchange.com/q/163134) rather than as plain text
