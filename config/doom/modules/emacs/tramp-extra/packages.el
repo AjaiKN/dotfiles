f1a905ae831058ebe466939f0d1ef8ac0840a014
Ajai Khatri Nelson <ajai@ajai.dev>
2026-02-19 15:28:35 -0500

    bump: :emacs tramp-extra
    ArthurHeymans/emacs-tramp-rpc@5126670acf97 -> ArthurHeymans/emacs-tramp-rpc@e0d0c4984e41
    
    Fixed a bug

--- config/doom/modules/emacs/tramp-extra/packages.el
+++ config/doom/modules/emacs/tramp-extra/packages.el
@@ -6,7 +6,7 @@
     :recipe (:host github :repo "jsadusk/tramp-hlo")))
 
 (when (modulep! +rpc)
-  (package! tramp-rpc :pin "5126670acf97abe0367857c642455a984efe019c"
+  (package! tramp-rpc :pin "e0d0c4984e4170dc599b87858b68eacf34838e09"
     :recipe (:host github :repo "ArthurHeymans/emacs-tramp-rpc"
              :files (:defaults "**/*")
              ;; TODO: remove this if tramp-rpc's autoloads are fixed (also from ./config.el)
