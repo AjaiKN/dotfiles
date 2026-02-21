6b930a9ce594641bceda60f854b107105b8df99d
Ajai Khatri Nelson <ajai@ajai.dev>
2026-02-21 03:49:11 -0500

    emacs: tempel: move snippets into separate files

--- config/doom/modules/editor/tempel/config.el
+++ config/doom/modules/editor/tempel/config.el
@@ -5,7 +5,7 @@
   :ghook ('(prog-mode-hook text-mode-hook conf-mode-hook) #'+tempel-setup-capf-h)
   :ghook ('doom-first-buffer-hook #'global-tempel-abbrev-mode)
   :config
-  (setq! tempel-path (list (expand-file-name "snippets.eld" doom-user-dir)))
+  (setq! tempel-path (list (expand-file-name "snippets/*.eld" doom-user-dir)))
   (add-to-list 'tempel-user-elements #'+tempel-add-user-elements)
 
   (setq! tempel-done-on-region nil)
