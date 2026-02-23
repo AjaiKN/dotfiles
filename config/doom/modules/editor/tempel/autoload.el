33573b0015da38b3b83a29befb216def6fdb349f
Ajai Khatri Nelson <ajai@ajai.dev>
2026-02-22 23:57:55 -0500

    emacs: tempel yas simplify

--- config/doom/modules/editor/tempel/autoload.el
+++ config/doom/modules/editor/tempel/autoload.el
@@ -53,15 +53,29 @@ string).  It returns t if a new completion is found, nil otherwise."
            for el in thing
            when (and el (not (equal el "")))
            do (if (and (stringp (car ret)) (stringp el))
-                  (setf (car ret) (concat (car ret) el))
+                  (setf (car ret) (concat el (car ret)))
                 (push el ret))
            finally return ret))
 
 (defun +tempel--read-sexp (&optional start)
-  (save-excursion
-    (goto-char (or start (point)))
-    (when-let* ((sexp (ignore-errors (read (current-buffer)))))
-      (cons sexp (point)))))
+  (save-match-data
+    (let ((orig (point)))
+      (goto-char (or start (point)))
+      (or
+       (ignore-errors
+         (read (current-buffer)))
+       (progn
+         (goto-char orig)
+         nil)))))
+
+(cl-defun +tempel--yas--scan-for-field-end ()
+  (save-match-data
+    (while (progn (or (re-search-forward "\\${\\|}" nil 'noerror)
+                      (cl-return nil))
+                  (when (eq (char-before) ?\{)
+                    ;; Nested field.
+                    (+tempel--yas--scan-for-field-end))))
+    t))
 
 (defun +tempel--parse-yasnippet-template-1 (str &optional nested-p)
   (require 'yasnippet)
@@ -72,78 +86,59 @@ string).  It returns t if a new completion is found, nil otherwise."
       (let ((contents nil)
             (transformer nil))
         (while (search-forward-regexp (rx (any "`$\\")) nil 'noerror)
+          (unless (eq (point) 2)
+            (push (delete-and-extract-region (point-min) (1- (point))) contents))
           (pcase (char-before)
-            (?\\ (when (memq (char-after (1+ (point))) '(?\{ ?\} ?\( ?\) ?\\ ?\` ?\" ?\' ?\$))
+            (?\\ (when (memq (char-after) '(?\{ ?\} ?\( ?\) ?\\ ?\` ?\" ?\' ?\$))
                    (delete-char -1)
                    (forward-char 1)))
-            (?\$ (backward-char)
-                 (cond ((looking-at "$\\([0-9]+\\)") ; e.g. $12
-                        (push (buffer-substring-no-properties (point-min) (point)) contents)
+            (?\$ (cond ((or (looking-at "\\([0-9]+\\)")    ; e.g. $12
+                            (looking-at "{\\([0-9]+\\)}")) ; e.g. ${12}
                         (push (list 'yas-field (string-to-number (match-string 1))) contents)
                         (delete-region (point-min) (match-end 0)))
-                       ((looking-at yas--field-regexp)
-                        (push (buffer-substring-no-properties (point-min) (point)) contents)
-                        (let* ((brace-scan (save-match-data
-                                             (goto-char (match-beginning 2))
-                                             (yas--scan-for-field-end)))
-                               (closing-brace 125)
-                               (real-match-end-0 (if (eq closing-brace (char-before brace-scan))
-                                                     brace-scan
-                                                   (point)))
-                               (number (and (match-string-no-properties 1)
-                                            (string-to-number (match-string-no-properties 1))))
-                               (field2 (match-string-no-properties 2)))
-                          (push (cond
-                                 ;; e.g. ${12}
-                                 ((and (not number) (string-match-p "\\`[0-9]+\\'" field2))
-                                  (list 'yas-field (string-to-number field2)))
-                                 ;; e.g. ${2:$(capitalize yas-text)}
-                                 ((and-let* (((string-match-p "\\`\\$[ \t\n]*(" field2))
-                                             (sexp+end (+tempel--read-sexp (1+ (match-beginning 2))))
-                                             (sexp (car sexp+end))
-                                             (end (cdr sexp+end)))
-                                    (list 'yas-mirror number sexp)))
-                                 ;; e.g. ${1:something}
-                                 (t
-                                  `(yas-field
-                                    ,number
-                                    ,@(+tempel--parse-yasnippet-template-1
-                                       (buffer-substring-no-properties (match-beginning 2)
-                                                                       (1- real-match-end-0))
-                                       'nested))))
-                                contents)
-                          (delete-region (point-min) real-match-end-0)
-                          (goto-char (point-min))))
+                       ((save-excursion
+                          (and-let* (((when (looking-at "{\\([0-9]+\\):")
+                                        (goto-char (match-end 0))
+                                        t))
+                                     (number (string-to-number (match-string-no-properties 1)))
+                                     (field2-beg (point))
+                                     ((+tempel--yas--scan-for-field-end))
+                                     (field2 (buffer-substring-no-properties field2-beg (1- (point)))))
+                            (push (if-let* (((string-match-p "\\`\\$[ \t\n]*(" field2))
+                                            (sexp (+tempel--read-sexp (1+ field2-beg))))
+                                      (progn
+                                        (forward-char)
+                                        ;; e.g. ${2:$(capitalize yas-text)}
+                                        `(yas-mirror ,number ,sexp))
+                                    ;; e.g. ${1:something}
+                                    `(yas-field
+                                      ,number
+                                      ,@(+tempel--parse-yasnippet-template-1
+                                         (buffer-substring-no-properties field2-beg (1- (point)))
+                                         'nested)))
+                                  contents)
+                            (delete-region (point-min) (point))
+                            t)))
                        ;; e.g. $(upcase yas-text)
                        ((save-excursion
                           (and-let* (((and nested-p (null transformer)))
-                                     ((looking-at (if (and (bobp) (null contents))
+                                     ((looking-at (if (null contents)
                                                       ;; e.g. ${1:mydefine$(upcase yas-text)}
-                                                      "\\$\\$[ \t\n]*\\(([^)]*)\\)"
+                                                      "\\$[ \t\n]*\\(([^)]*)\\)"
                                                     ;; e.g. ${1:$$(upcase yas-text)}
-                                                    "\\$[ \t\n]*\\(([^)]*)\\)")))
-                                     (sexp+end (+tempel--read-sexp (match-beginning 1)))
-                                     (sexp (car sexp+end))
-                                     (end (cdr sexp+end)))
-                            (push (buffer-substring-no-properties (point-min) (point)) contents)
+                                                    "[ \t\n]*\\(([^)]*)\\)")))
+                                     (sexp (+tempel--read-sexp (match-beginning 1))))
                             (setq transformer sexp)
-                            (delete-region (point-min) end)
+                            (delete-region (point-min) (point))
                             t)))
                        ;; e.g. $>
-                       ((looking-at (rx "$>"))
-                        (push (buffer-substring-no-properties (point-min) (point)) contents)
+                       ((looking-at (rx ">"))
                         (push 'yas-indent contents)
-                        (delete-region (point-min) (match-end 0)))
-                       (t
-                        (forward-char))))
-            (?\` (when-let* ((beg (point))
-                             (sexp+end (+tempel--read-sexp))
-                             (sexp (car sexp+end))
-                             (end (cdr sexp+end))
-                             (end-char (char-after end))
-                             ((eq end-char ?\`)))
-                   (push (buffer-substring-no-properties (point-min) (1- (point))) contents)
-                   (delete-region (point-min) (1+ end))
-                   (push (list 'yas-subst sexp) contents)))))
-        (push (buffer-string) contents)
+                        (delete-region (point-min) (match-end 0)))))
+            (?\` (when-let* ((sexp (+tempel--read-sexp))
+                             ((eq (char-after) ?\`)))
+                   (forward-char)
+                   (push `(yas-subst ,sexp) contents)
+                   (delete-region (point-min) (point))))))
+        (push (buffer-substring-no-properties (point-min) (point-max)) contents)
         (list (+tempel--simplify-yasnippet-template contents) transformer)))))
