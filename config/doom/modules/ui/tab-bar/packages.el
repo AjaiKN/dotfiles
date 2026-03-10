24bba82b1d5f1e91bb6f272300bcbe83b800bc60
Ajai Khatri Nelson <ajai@ajai.dev>
2026-03-10 02:21:47 -0400

    emacs: update bufferlo (my PR was accepted)

--- config/doom/modules/ui/tab-bar/packages.el
+++ config/doom/modules/ui/tab-bar/packages.el
@@ -3,7 +3,7 @@
 
 (package! tab-bar :type 'built-in)
 (if (modulep! +bufferlo)
-    (package! bufferlo :pin "ee7db7237b152a06b01375bbf57b2965a354ba9f"
+    (package! bufferlo :pin "a4971b52232ff622f768fcd569c3ae5929580f3f"
       :recipe (:host github :repo "florommel/bufferlo"))
   (package! tabspaces :pin "cd9f780bd69747fb8a598be1d4347e2e161c2d82"))
 (package! activities :pin "5025962126d140a7e26d36c3a2750bf4ff0bfd45"
