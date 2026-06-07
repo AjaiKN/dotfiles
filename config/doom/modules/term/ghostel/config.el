2094837d67b4abd84e178bfe15f81ff67b1f6ade
Ajai Khatri Nelson <ajai@ajai.dev>
2026-06-07 02:19:20 -0400

    emacs: ghostel:

--- config/doom/modules/term/ghostel/config.el
+++ config/doom/modules/term/ghostel/config.el
@@ -24,22 +24,39 @@
   :after (ghostel evil)
   :hook (ghostel-mode . evil-ghostel-mode))
 
-(use-package! ghostel-compile
-  :defer t
-  :after-call compilation-start compile recompile
-  :init
-  (after! (:and compile ghostel) (require 'ghostel-compile))
-  (define-advice compilation-start (:around (fn &rest args) +ghostel-compile-global-a -95)
-    "Lazy load ghostel-compile when compilation-start is first called."
-    (if (featurep 'ghostel-compile)
-        (apply fn args)
-      (require 'ghostel-compile)
-      (apply #'compilation-start args)))
-  (map! [remap compile]   #'ghostel-compile
-        [remap recompile] #'ghostel-recompile)
-  :config
-  (advice-remove #'compilation-start 'compilation-start@+ghostel-compile-global-a)
-  (ghostel-compile-global-mode)
-  (when (modulep! :ui popup +defaults)
-    (set-popup-rule! "^\\*ghostel-compil"
-      :vslot -2 :size 0.3 :autosave t :quit t :ttl 0)))
+;; TODO: disable xterm-color before enabling these:
+
+;; (use-package! ghostel-compile
+;;   :defer t
+;;   :after-call compilation-start compile recompile
+;;   :init
+;;   (after! (:and compile ghostel) (require 'ghostel-compile))
+;;   (define-advice compilation-start (:around (fn &rest args) +ghostel-compile-global-a -95)
+;;     "Lazy load ghostel-compile when compilation-start is first called."
+;;     (if (featurep 'ghostel-compile)
+;;         (apply fn args)
+;;       (require 'ghostel-compile)
+;;       (apply #'compilation-start args)))
+;;   (map! [remap compile]   #'ghostel-compile
+;;         [remap recompile] #'ghostel-recompile)
+;;   :config
+;;   (advice-remove #'compilation-start 'compilation-start@+ghostel-compile-global-a)
+;;   (ghostel-compile-global-mode)
+;;   (when (modulep! :ui popup +defaults)
+;;     (set-popup-rule! "^\\*ghostel-compil"
+;;       :vslot -2 :size 0.3 :autosave t :quit t :ttl 0)))
+
+;; (use-package! ghostel-eshell
+;;   :when (modulep! :term eshell)
+;;   :ghook ('eshell-load-hook #'ghostel-eshell-visual-command-mode))
+
+;; (use-package! ghostel-comint
+;;   :ghook 'shell-mode-hook
+
+;;   :after-call comint-mode-hook
+;;   :config
+;;   (ghostel-comint-global-mode 1)
+
+;;   (add-hook! '(shell-mode-hook ghostel-comint-mode-hook)
+;;     (font-lock-mode -1)
+;;     (setq-local font-lock-function #'ignore)))
