1c5062cc417618eb5a7540a03b56fa00f858a401
Ajai Khatri Nelson <ajai@ajai.dev>
2026-06-12 09:37:00 -0400

    zsh: make history backup more portable

--- config/zsh/plugins/history.zsh
+++ config/zsh/plugins/history.zsh
@@ -4,16 +4,14 @@ export HISTSIZE=5000000000
 export SAVEHIST=1000000000
 export HISTFILE="$HOME/.zsh_my_history"
 
-# if ruby is installed
-if (( $+commands[ruby] )); then
+if (( $+commands[awk] )); then
 	# echo_if_interactive 'Backing up ~/.zsh_my_history to ~/.zsh_my_history_backup'
-	# append all lines in ~/.zsh_my_history that are not in ~/.zsh_my_history_backup to ~/.zsh_my_history_backup
-	# Alternatively, this might work (from ChatGPT): awk 'NR==FNR {seen[$0]; next} !($0 in seen)' "$HOME/.zsh_my_history_backup" "$HOME/.zsh_my_history"
 	(
 		umask go-rwx
-		MISE_NOT_FOUND_AUTO_INSTALL=false ruby -e "puts File.readlines('$HOME/.zsh_my_history') - File.readlines('$HOME/.zsh_my_history_backup')" >>| ~/.zsh_my_history_backup &|
+		# append all lines in ~/.zsh_my_history that are not in ~/.zsh_my_history_backup to ~/.zsh_my_history_backup
+		awk 'FILENAME==ARGV[1]{seen[$0]=1; next} !($0 in seen)' "$HOME/.zsh_my_history_backup" "$HOME/.zsh_my_history" >>| "$HOME/.zsh_my_history_backup" &|
 	)
 	# echo_if_interactive 'Done backing up ~/.zsh_my_history'
 else
-	echo_if_interactive 'ruby not installed, not backing up ~/.zsh_my_history to ~/.zsh_my_history_backup'
+	echo_if_interactive 'awk not available, not backing up ~/.zsh_my_history to ~/.zsh_my_history_backup'
 fi
